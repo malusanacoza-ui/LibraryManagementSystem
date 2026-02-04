04c26652ffd36b1bd467733a70c32299a48fd4ff
malusanacoza-ui <malusana.co.za@gmail.com>
2026-02-04 10:33:46 +0200

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    [Required]
+    public string Author { get; set; }
+
+    public string Category { get; set; }
+
+    public bool IsAvailable { get; set; } = true;
+}
